6d54df185ec337c14f15ac863826cdc09a9be336
Chathumi  Perera <chathumiamarasinghe@gmail.com>
2024-08-10 22:16:42 +0530

    login page updated
    login page frontend with styles updated

---
+++ New folder/Login/login.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+      crossorigin="anonymous"
+    />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="login.css" />
+</head>
+<body>
+    <section class="background-radial-gradient" >
+    <div class="container" style="width:500px">
+        <div class="card m-5">
+            <div class="card-body">
+                <form action="" method="post">
+
+                    <div class="form-group mb-4">
+                        <input type="text" class="form-control">
+                        <label for="inputusername">Username</label>
+                    </div>
+
+                    <div class="form-group mb-4">
+                        <input type="password" class="form-control">
+                        <label for="inputpassword">Password</label>
+                    </div>
+
+                    <div class="d-flex justify-content-center">
+                        <button type="submit" class="btn custom-signin-btn mb-4 me-4">Login</button>
+                    </div>
+
+                    <div class="text-center d-flex justify-content-center align-items-center">
+                                 <p class="mb-0 me-2"> haven't an account</p>
+                                   <a href="#" class="btn custom-signin-btn mb-2">
+                                    Sign Up
+                                   </a>
+                    </div>
+
+                    <div class="text-center">
+                                    <p>or sign in with:</p>
+                                    <a href="https://www.facebook.com" class="btn btn-link btn-floating mx-1" target="_blank" aria-label="Facebook">
+                                        <i class="fab fa-facebook-f"></i>
+                                    </a>
+                                    <a href="https://www.google.com" class="btn btn-link btn-floating mx-1" target="_blank" aria-label="Google">
+                                        <i class="fab fa-google"></i>
+                                    </a>
+                                    <a href="https://twitter.com" class="btn btn-link btn-floating mx-1" target="_blank" aria-label="Twitter">
+                                        <i class="fab fa-twitter"></i>
+                                    </a>
+                                    <a href="https://github.com" class="btn btn-link btn-floating mx-1" target="_blank" aria-label="GitHub">
+                                        <i class="fab fa-github"></i>
+                                    </a>
+                    </div>
+
+                </form>
+
+            </div>
+        </div>
+
+   
+    
+   </div>                             
+    
+</section>                           
+
+<script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+      crossorigin="anonymous"
+    ></script>  
+</body>
+</html>
