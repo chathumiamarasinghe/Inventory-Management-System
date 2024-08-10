7143f71cca52c9b5e656372f262c335223f1d33a
Chathumi  Perera <chathumiamarasinghe@gmail.com>
2024-08-11 02:04:13 +0530

    login frontend completed
    frontend designs completed for login page

--- New folder/Login/login.php
+++ New folder/Login/login.php
@@ -15,11 +15,24 @@
     <link rel="stylesheet" href="login.css" />
 </head>
 <body>
-    <section class="background-radial-gradient" >
-    <div class="container" style="width:500px">
-        <div class="card m-5">
-            <div class="card-body">
-                <form action="" method="post">
+<section class="background-radial-gradient >
+    <div class="container px-5 py-5 px-md-5 text-center text-lg-start my-5">
+        <div class="row gx-lg-5 align-items-center mb-5">
+                <div class="col-lg-6 mb-5 mb-lg-0" style="z-index: 10">
+                    <h1 class="my-5 display-5 fw-bold ls-tight" style="color: hsl(218, 81%, 95%)">
+                        Welcome Back <br />
+                        <span style="color: hsl(218, 81%, 75%)">Login to Your Account</span>
+                    </h1>
+                    <p class="mb-4 opacity-70" style="color: hsl(218, 81%, 85%)">
+                        Please enter your email and password to log in. If you don't have an account, you can sign up.
+                    </p>
+                </div>
+            <div class="col-lg-6 mb-5 mb-lg-0 position-relative">
+                    <div id="radius-shape-1" class="position-absolute shadow-5-strong"></div>
+                    <div id="radius-shape-2" class="position-absolute shadow-5-strong"></div>
+            <div class="card m-5 px-4 py-5" style="width:500px">
+                <div class="card-body">
+                  <form action="" method="post">
 
                     <div class="form-group mb-4">
                         <input type="text" class="form-control">
@@ -58,16 +71,16 @@
                                     </a>
                     </div>
 
-                </form>
+                 </form>
 
+                </div>
+            </div>
             </div>
         </div>
-
-   
     
-   </div>                             
+    </div>                             
     
-</section>                           
+    </section>                           
 
 <script
       src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
