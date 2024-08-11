680853643cd6f085c79807687bfec54c17a005d4
Chathumi  Perera <chathumiamarasinghe@gmail.com>
2024-08-11 23:30:36 +0530

    sign up frontend updated
    sign up frontend updated

--- New folder/Login/login.php
+++ New folder/Login/login.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>DocumentL</title>
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
       rel="stylesheet"
--- New folder/Register/register.php
+++ New folder/Register/register.php
@@ -3,9 +3,93 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>DocumentR</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+      crossorigin="anonymous"
+    />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="register.css" />
 </head>
 <body>
+<section class="background-radial-gradient >
+    <div class="container px-5 py-5 px-md-5 text-center text-lg-start my-5">
+        <div class="row gx-lg-5 align-items-center mb-5">
+                <div class="col-lg-6 mb-5 mb-lg-0" style="z-index: 10">
+                    <h1 class="my-5 display-5 fw-bold ls-tight" style="color: hsl(218, 81%, 95%)">
+                        Join Us Today <br />
+                        <span style="color: hsl(218, 81%, 75%)">Create Your Account</span>
+                    </h1>
+                    <p class="mb-4 opacity-70" style="color: hsl(218, 81%, 85%)">
+                        Fill the details below to create a new account.If you already have an account,you can log in instead.
+                    </p>
+                </div>
+            <div class="col-lg-6 mb-5 mb-lg-0 position-relative">
+                    <div id="radius-shape-1" class="position-absolute shadow-5-strong"></div>
+                    <div id="radius-shape-2" class="position-absolute shadow-5-strong"></div>
+            <div class="card m-5 px-4 py-5" style="width:500px">
+                <div class="card-body">
+                  <form action="" method="post">
+
+                    <div class="form-group mb-4">
+                        <input type="text" class="form-control">
+                        <label for="inputusername">Username</label>
+                    </div>
+
+                    <div class="form-group mb-4">
+                        <input type="email" class="form-control">
+                        <label for="inputemail">Email Address</label>
+                    </div>
+
+                    <div class="form-group mb-4">
+                        <input type="password" class="form-control">
+                        <label for="inputpassword">Password</label>
+                    </div>
+
+                    <div class="form-group mb-4">
+                        <input type="password" class="form-control">
+                        <label for="inputconfirmpassword">Confirm Password</label>
+                    </div>
+
+                    <div class="d-flex justify-content-center">
+                        <button type="submit" class="btn custom-signin-btn mb-4 me-4">Sign Up</button>
+                    </div>
+
+                    <div class="text-center d-flex justify-content-center align-items-center">
+                                 <p class="mb-0 me-2">Already have an account</p>
+                                   <a href="#" class="btn custom-signin-btn mb-2">
+                                    Sign In
+                                   </a>
+                    </div>
+
+                    <div class="text-center">
+                                    <p>or sign in with:</p>
+                                    <a href="https://www.facebook.com" class="btn btn-link btn-floating mx-1" target="_blank" aria-label="Facebook">
+                                        <i class="fab fa-facebook-f"></i>
+                                    </a>
+                                    <a href="https://www.google.com" class="btn btn-link btn-floating mx-1" target="_blank" aria-label="Google">
+                                        <i class="fab fa-google"></i>
+                                    </a>
+                                    <a href="https://twitter.com" class="btn btn-link btn-floating mx-1" target="_blank" aria-label="Twitter">
+                                        <i class="fab fa-twitter"></i>
+                                    </a>
+                                    <a href="https://github.com" class="btn btn-link btn-floating mx-1" target="_blank" aria-label="GitHub">
+                                        <i class="fab fa-github"></i>
+                                    </a>
+                    </div>
+
+                 </form>
+
+                </div>
+            </div>
+            </div>
+        </div>
     
+    </div>                             
+    
+    </section>
 </body>
 </html>
